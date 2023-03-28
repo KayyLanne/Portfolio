45b0e63fa861b77098fd765b5c6b987f5b6352cc
KayyLanne <kaylanne.regina@Hotmail.com>
2023-03-28 12:36:34 -0300

    adicionando sobre mim e habilidades

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 const header = document.querySelector("header");
 function activateScroll(){
-    header.classList.toggle("sticky", scrollY > 100);
+    header.classList.toggle("ativo", scrollY > 100);
 }
 
 window.addEventListener("scroll", activateScroll);
