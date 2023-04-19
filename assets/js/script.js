f587b4ed6075238dc712d56f784d88dac85a280a
KayyLanne <kaylanne.regina@hotmail.com>
2023-04-19 19:04:04 -0300

    add outros modals

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,31 @@ const toggleModal = () => {
 [openModalButton, closeModalButton, fade].forEach((el) => {
     el.addEventListener("click", () => toggleModal());
 })
+
+const openModalButton2 = document.querySelector("#open-modal2");
+const closeModalButton2 = document.querySelector("#close-modal2");
+const modal2 = document.querySelector("#modal2");
+const fade2 = document.querySelector("#fade2");
+
+const toggleModal2 = () => {
+    modal2.classList.toggle("hide");
+    fade2.classList.toggle("hide");
+}
+
+[openModalButton2, closeModalButton2, fade2].forEach((el) => {
+    el.addEventListener("click", () => toggleModal2());
+})
+
+const openModalButton3 = document.querySelector("#open-modal3");
+const closeModalButton3 = document.querySelector("#close-modal3");
+const modal3 = document.querySelector("#modal3");
+const fade3 = document.querySelector("#fade3");
+
+const toggleModal3 = () => {
+    modal3.classList.toggle("hide");
+    fade3.classList.toggle("hide");
+}
+
+[openModalButton3, closeModalButton3, fade3].forEach((el) => {
+    el.addEventListener("click", () => toggleModal3());
+})
